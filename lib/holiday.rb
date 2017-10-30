0054eea71e595d7c5dd180d9121f9441bd5b41d0
Jen Poole <contact.vandepool@gmail.com>
2017-10-30 20:21:53 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,5 +1,6 @@
 require 'pry'
 
+# return the second element in the 4th of July array
 def second_supply_for_fourth_of_july(holiday_hash)
   # given that holiday_hash looks like this:
   # {
@@ -17,34 +18,50 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #     :memorial_day => ["BBQ"]
   #   }
   # }
-  # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
+
+# iterates through winter holidays adds a supply to each one
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
+  # add the second argument, which is a supply, to BOTH the Christmas AND the New Year's arrays
 
+  winter = holiday_hash[:winter]
+
+  winter.each do |holiday|
+    holiday.each do
+      holiday[1] << "Balloons"
+    end
+  end
 end
 
 
+# adds a supply to memorial day
+# adds a different supply to memorial day
 def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
-
+  # again, holiday_hash is the same as the ones above. add the second argument (supply) to the memorial day array in holiday_hash
+  holiday_hash[:spring][:memorial_day] << supply
 end
 
+
+# modifies the original hash by adding supplies of a new holiday to a season
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
+  holiday_hash[season][holiday_name] = supply_array # note: supply_array is already an array
   # remember to return the updated hash
-
+  holiday_hash
 end
 
+
+# has all the winter supplies
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].values.flatten
 end
 
+
+# should output the formatted list of holidays and their supplies
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
@@ -53,18 +70,18 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
 end
 
+
+# should return holidays that have a value of "BBQ" (:fourth_of_july and :memorial_day)
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-
+  # return an array of holiday names (as symbols) where supply lists include the string "BBQ"
+  holiday_hash.map do |season, holiday| # use .map instead of .each to return new altered array
+    holiday.map do |holiday_name, supply|
+      holiday_name if supply.include? "BBQ"
+    end
+  end.flatten.compact
+  # use .flatten to return single array instead of nested array:
+  # [nil, nil, :fourth_of_july, nil, :memorial_day] --> instead of --> [[nil, nil], [:fourth_of_july], [nil], [:memorial_day]]
+  # remove nil values using .compact method: https://stackoverflow.com/questions/13485468/map-and-remove-nil-values-in-ruby
 end
-
-
-
-
-
-
-
